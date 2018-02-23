664adfea0876b7e4156bdb09811302936ac31ce6
Clyde DCruz <clydcruz@in.ibm.com>
2018-02-23 11:32:55 +0530

    included example of displaying bbox for detection

--- detect.py
+++ detect.py
@@ -1,29 +1,29 @@
 import base64
-try:
-    from StringIO import StringIO
-except ImportError:
-    from io import StringIO
-#import cStringIO
-import sys
-
-PATH_TO_CKPT = 'frozen_inference_graph.pb'
-PATH_TO_LABELS = 'label_map.pbtxt'
-MODEL_BASE = 'D:\Python\ML\models\research'
-sys.path.append(MODEL_BASE)
-sys.path.append(MODEL_BASE + '\object_detection')
-sys.path.append(MODEL_BASE + '\slim')
-
 import numpy as np
 from PIL import Image
 from PIL import ImageDraw
 import tensorflow as tf
+import sys
+
+try:
+    from StringIO import StringIO
+except ImportError:
+    from io import StringIO
+
+PATH_TO_CKPT = 'inference_graph\frozen_inference_graph.pb'
+PATH_TO_LABELS = 'inference_graph\label_map.pbtxt'
+MODEL_BASE = 'D:\Python\ML\models\research'
+sys.path.append(MODEL_BASE)
+sys.path.append(MODEL_BASE + '\object_detection')
+sys.path.append(MODEL_BASE + '\slim')
 from utils import label_map_util
 
 PATH_TO_CKPT = 'frozen_inference_graph.pb'
 PATH_TO_LABELS = 'label_map.pbtxt'
 
+
 class ObjectDetector(object):
-    
+
   def __init__(self):
     self.detection_graph = self._build_graph()
     self.sess = tf.Session(graph=self.detection_graph)
@@ -49,6 +49,15 @@ class ObjectDetector(object):
     return np.array(image.getdata()).reshape(
         (im_height, im_width, 3)).astype(np.uint8)
 
+  def draw_bounding_box_on_image(self,image, box, color='red', thickness=4):
+        draw = ImageDraw.Draw(image)
+        im_width, im_height = image.size
+        ymin, xmin, ymax, xmax = box
+        (left, right, top, bottom) = (xmin * im_width, xmax * im_width,
+                                        ymin * im_height, ymax * im_height)
+        draw.line([(left, top), (left, bottom), (right, bottom),
+                    (right, top), (left, top)], width=thickness, fill=color)
+
   def detect(self, image):
     image_np = self._load_image_into_numpy_array(image)
     image_np_expanded = np.expand_dims(image_np, axis=0)
@@ -73,13 +82,22 @@ def detect_objects(image_path):
     image = Image.open(image_path).convert('RGB')
     boxes, scores, classes, num_detections = detector.detect(image)
     image.thumbnail((480, 480), Image.ANTIALIAS)
-  
+    # # show a bounding box 
+    # detector.draw_bounding_box_on_image(image,boxes[0],thickness=int(scores[0]*10)-4)
+    # image.show()
 
-    print("boxes:", boxes[0])
-    print("scores:", scores[0])
-    print("classes:", classes[0])
+
+    # print("boxes:", boxes[0])
+    # print("scores:", classes[0])
+    # print("classes:", classes[0])
+    # print("num_detections:", num_detections)
+
+    print("\n ", image_path,"\t",scores[0])
 
 
 detector = ObjectDetector()
 
-detect_objects("training_img0.jpg")
+TEST_IMAGE_DIR="test_images"
+for i in range(6,10):
+    detect_objects(TEST_IMAGE_DIR+"/"+"image"+str(i)+".jpg")
+
